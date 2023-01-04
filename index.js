6b92c168c166c7d193bc46c19a3434ce0b42730c
NerdyTechy <techy@techy.lol>
2023-01-04 18:46:23 +0000

    Initial workflow

---
+++ index.js
@@ -0,0 +1,23 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+(async () => {
+    try {
+        const timeLimit = core.getInput("time-limit");
+        const base = core.getInput("base");
+        const endpoints = core.getInput("endpoints");
+    
+        for (endpoint of endpoints){
+            const time = Date.now();
+            await fetch(`${base}${endpoint}`).then(data => data.text());
+            const timeTaken = Date.now() - time;
+            if (timeTaken >= timeLimit){
+                core.setFailed(`The endpoint ${endpoint} took ${timeTaken}ms to respond when limit was set to ${timeLimit}ms.`);
+            }
+        }
+    } catch(err){
+        core.setFailed(err.message);
+    }
+})();
